39e53f3b8c6ab000bc01454c76f22f3293253efd
arogozin <aleksandr@rogozin.us>
2014-04-04 15:07:49 -0400

    Cleaning Up

---
+++ run.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import urllib2
+import re
+import codecs
+from bs4 import BeautifulSoup
+import json
+from multiprocessing import Lock, Process, Queue, current_process
+import unicodedata
+
+class WebPage(object):
+    
+    url = ''
+    html = ''
+    soup = ''
+    result = ''
+    
+    def __init__(self, url):
+        self.url = url
+        self.html = self.retrievePage(self.url)
+        self.soup = self.parsePage(self.html)
+        
+    def retrievePage(self, url):
+        request = urllib2.Request(url)
+        request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36')
+        response = urllib2.urlopen(request)
+        
+        if (response.code == 200):
+            return response.read()
+        else:
+            return 'Error.'
+
+    def getReviewers(self):
+        reviewers = Queue()
+        [reviewers.put(item.split(' (')[0]) for item in self.soup.find_all('p')[-1].get_text().split('\n')]        
+        return reviewers
+
+    def prepareResult(self, url, soup):
+        result = {}
+        result['url'] = url
+        result['title'] = soup.title.get_text()#.encode('ascii', 'ignore')
+        
+        return result
+
+    def parsePage(self, html):
+        soup = BeautifulSoup(html)
+        
+        return soup
+
+    def getSoup(self):
+        return self.soup
+    
+def getAuthorProfileUrl(name, splitName = None, url = None):
+    searchBaseUrl = 'http://papers.nips.cc/search/?q='
+    name = str(name).decode('utf-8')
+    name = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore')
+    name = re.sub(r'["|\'][a-zA-Z]+["|\']', '', name)
+    
+    if splitName is None:
+        splitName = name.split(' ')
+    
+    if url is None:
+        query = '+'.join(splitName)#name.replace(' ', '+')
+        searchUrl = searchBaseUrl + query
+        
+        results = WebPage(searchUrl)
+        soup = results.getSoup()
+        
+        if soup.h4 is not None:
+            if soup.h4.a is not None:
+                url = soup.h4.a.get('href')
+    
+    authorUrlName = '-'.join(splitName).replace(' ', '-').replace('.', '').encode('utf-8').decode('utf-8')
+    authorUrlName = unicodedata.normalize('NFKD', authorUrlName).encode('ascii', 'ignore')
+    authorUrlName = authorUrlName.lower()
+    
+    if len(splitName) == 1:
+        if authorUrlName in url:
+            return url
+    elif len(splitName) == 2:
+        if authorUrlName in url:
+            return url
+        else:
+            return getAuthorProfileUrl(name, [splitName[-1]])
+    elif len(splitName) == 3:
+        if authorUrlName in url:
+            return url
+        else:
+            result = getAuthorProfileUrl(name, [splitName[0], splitName[1]])
+            if (result is None):
+                result = getAuthorProfileUrl(name, [splitName[0], splitName[2]])
+            if (result is None):
+                result = getAuthorProfileUrl(name, [splitName[1], splitName[2]])
+            if (result is None):
+                return getAuthorProfileUrl(name, [splitName[-1]])
+            else:
+                return result
+    elif len(splitName) == 4:
+        if authorUrlName in url:
+            return url
+        else:
+            result = getAuthorProfileUrl(name, [splitName[0], splitName[1], splitName[2]])
+            if (result is None):
+                result = getAuthorProfileUrl(name, [splitName[0], splitName[1], splitName[3]])
+            if (result is None):
+                result = getAuthorProfileUrl(name, [splitName[0], splitName[2], splitName[3]])
+            if (result is None):
+                result = getAuthorProfileUrl(name, [splitName[1], splitName[2], splitName[3]])
+            if (result is None):
+                return getAuthorProfileUrl(name, [splitName[-1]])
+            else:
+                return result
+    else:
+        return None
+
+print getAuthorProfileUrl('Miguel Carreira-Perpiñán')
+
+#print getAuthorProfileUrl(name, url)
+
+page = WebPage('https://nips.cc/Conferences/2012/Committees')
+reviewers = page.getReviewers()
+
+#for reviewer in iter(reviewers.get, 'STOP'):
+#    print reviewer
+#    print searchReviewerProfile(reviewer)
+#    print
